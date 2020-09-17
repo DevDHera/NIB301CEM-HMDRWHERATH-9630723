861f8c5b2690f1411b99b314d82b4eb75808ef4d
Devin Herath <devdhera@gmail.com>
2020-09-17 05:44:09 -0700

    Basic navigation setup

--- HMDRWHERATH_9630723_COBSCCOMP191P-008/Controllers/HomeViewContoller.swift
+++ HMDRWHERATH_9630723_COBSCCOMP191P-008/Controllers/HomeViewContoller.swift
@@ -11,8 +11,12 @@ import UIKit
 class HomeViewContoller: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        super.viewDidLoad()        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.tabBarController?.tabBar.isHidden = false
     }
 
 

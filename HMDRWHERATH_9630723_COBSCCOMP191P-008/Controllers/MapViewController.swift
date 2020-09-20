83309b7c8903b1386bb51b0dd4330565c9d2ab4b
Devin Herath <devdhera@gmail.com>
2020-09-20 07:46:32 -0700

    Added covid 19 suspected location banner to the map

--- HMDRWHERATH_9630723_COBSCCOMP191P-008/Controllers/MapViewController.swift
+++ HMDRWHERATH_9630723_COBSCCOMP191P-008/Controllers/MapViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import MapKit
 import CoreLocation
 import Firebase
+import SCLAlertView
 
 class MapViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var mapView: MKMapView!
@@ -21,6 +22,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
     var userDocRefId = ""
     
     var geoPoints: [GeoPoint] = []
+    var currentUserGeoPoint: GeoPoint?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -52,12 +54,13 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
     
     func render(_ location: CLLocation) {
         let coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
         let region = MKCoordinateRegion(center: coordinate, span: span)
         mapView.setRegion(region, animated: true)
         let pin = MKPointAnnotation()
         pin.coordinate = coordinate
         mapView.addAnnotation(pin)
+        currentUserGeoPoint = GeoPoint(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
     }
     
     func updateLocations(_ location: CLLocation) {
@@ -85,17 +88,29 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
     
     func fetchUsers() {
         geoPoints = []
+        
         db.collection(Constants.UserStore.collectionName).addSnapshotListener { (querySnapshot, error) in
+            var infected = false
+            
             if let e = error {
                 print(e.localizedDescription)
             } else {
                 
                 if let snapshotDocuemnts = querySnapshot?.documents {
+                    
                     for doc in snapshotDocuemnts {
                         let data = doc.data()
                         if let geopoint = data[Constants.UserStore.location] as? GeoPoint {
                             self.geoPoints.append(geopoint)
+                            
+                            if self.currentUserGeoPoint != nil, let covidStatus = data[Constants.UserStore.covidStatus] as? String {
+                                if self.getRange(distance: 1.0, point: geopoint), covidStatus == UserCovidStatus.INFECTED.rawValue {
+                                    infected = true
+                                }
+                            }
+                            
                         }
+                        
                     }
                     DispatchQueue.main.async {
                         for i in self.geoPoints{
@@ -107,7 +122,10 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
                                 point.coordinate = CLLocationCoordinate2D(latitude: latitude as! CLLocationDegrees, longitude: longitude as! CLLocationDegrees)
                                 self.mapView.addAnnotation(point)
                             }
-  
+                        }
+                        
+                        if infected {
+                            SCLAlertView().showInfo("Danger", subTitle: "You are in a COVID 19 suspected location")
                         }
                     }
                 }
@@ -115,5 +133,21 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         }
     }
     
-    
+    func getRange(distance: Double, point: GeoPoint) -> Bool {
+//        ~1 mile of lat and lon in degrees
+        let lat = 0.0144927536231884
+        let lon = 0.0181818181818182
+        
+        let lowerLat = currentUserGeoPoint!.latitude - (lat * distance)
+        let lowerLon = currentUserGeoPoint!.longitude - (lon * distance)
+
+        let greaterLat = currentUserGeoPoint!.latitude + (lat * distance)
+        let greaterLon = currentUserGeoPoint!.longitude + (lon * distance)
+        
+        if point.latitude > lowerLat, point.latitude < greaterLat, point.longitude > lowerLon, point.longitude < greaterLon {
+            return true
+        }
+        
+        return false
+    }
 }
